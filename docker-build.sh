483d1aa6696260bf1a3412485b485f7697857a48
felipedemacedo <felipedemacedo.cin@gmail.com>
2020-06-08 00:28:05 -0300

    add Docker build and run files

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t felipederodrigues/selenium-url-loader:latest .
---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run felipederodrigues/selenium-url-loader:latest http://www.google.com
